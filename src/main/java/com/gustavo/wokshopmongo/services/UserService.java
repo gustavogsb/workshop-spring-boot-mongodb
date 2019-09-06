07fa34ebd8af48c7cfa97826b65d312ad672d091
Gustavo Bastos <gustavogbs@gmail.com>
2019-09-06 19:00:35 -0300

    Obtendo um usuário por id

--- src/main/java/com/gustavo/wokshopmongo/services/UserService.java
+++ src/main/java/com/gustavo/wokshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.gustavo.wokshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gustavo.wokshopmongo.domain.User;
 import com.gustavo.wokshopmongo.repository.UserRepository;
+import com.gustavo.wokshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,11 @@ public class UserService {
 
 		public List<User> findAll(){
 			return repo.findAll();
+		}
+		
+		
+		public User findById(String id) {
+			Optional<User> obj = repo.findById(id);
+			return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 			
 		}
 }
