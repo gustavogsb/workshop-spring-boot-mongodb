1445144b2d23cbbc55a6add0cca7f19e3c18cbda
Gustavo Bastos <gustavogbs@gmail.com>
2019-09-18 23:08:25 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/gustavo/wokshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.gustavo.wokshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gustavo.wokshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+	
+}
